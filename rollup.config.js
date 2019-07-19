3f4728d5a06453215594d091543f7c1578a12906
Alexander Wunschik <alex@wunschik.net>
2019-07-19 23:03:26 +0200

    feat: also generate ESM module

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,37 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import nodeBuiltins from 'rollup-plugin-node-builtins';
 import babel from 'rollup-plugin-babel';
-import sourcemaps from 'rollup-plugin-sourcemaps';
 import { uglify } from "rollup-plugin-uglify";
 import banner from 'rollup-plugin-banner';
 import genHeader from './lib/header';
 
-const genSourceMap = false;
-
 export default [{
 	input: 'index.js',
 	output: {
-		file: 'dist/vis-timeline-graph2d.min.js',
-		name: 'vis',
-		exports: 'named',
-		format: 'umd',
-		sourcemap: genSourceMap
+		file: 'dist/vis-timeline-graph2d.esm.js',
+		format: 'esm',
 	},
 	plugins: [
 		commonjs(),
 		nodeBuiltins(),
 		nodeResolve(),
 		babel(),
-		sourcemaps(),
-		uglify({
-			sourcemap: genSourceMap
-		}),
+		banner(genHeader('timeline-graph2d'))
+	],
+}, {
+	input: 'index.js',
+	output: {
+		file: 'dist/vis-timeline-graph2d.min.js',
+		name: 'vis',
+		exports: 'named',
+		format: 'umd'
+	},
+	plugins: [
+		commonjs(),
+		nodeBuiltins(),
+		nodeResolve(),
+		babel(),
+		uglify(),
 		banner(genHeader('timeline-graph2d'))
 	]
-}]
+}];
