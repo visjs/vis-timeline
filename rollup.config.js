6082a84314d74a7e1b1ce06df9d4baf63aa0be75
Yotam Berkowitz <yotamberk@gmail.com>
2019-08-02 19:34:12 +0300

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default [{
 	output: {
 		file: 'dist/vis-timeline-graph2d.min.js',
 		name: 'vis',
-		exports: 'named',
+		exports: 'default',
 		format: 'umd',
 		banner: genHeader('timeline-graph2d'),
 		sourcemap: true,
