f4c2d6d4716221222f45cbcba575955042975bd1
Alexander Wunschik <alex@wunschik.net>
2023-03-13 09:31:13 +0100

    refactor(#672): implement native add and subtract helper functions

--- test/date-utils.test.js
+++ test/date-utils.test.js
@@ -1,6 +1,6 @@
 var assert = require("assert");
 
-var { getDayOfYear, setDayOfYear } = require("../lib/date-utils");
+var { getDayOfYear, setDayOfYear, add, subtract } = require("../lib/date-utils");
 var moment = require("moment");
 
 describe("date-utils", function() {
@@ -89,6 +89,88 @@ describe("date-utils", function() {
         assert.throws(() => {
           setDayOfYear(new Date(date), dayOfYear);
         }, new RangeError(`dayOfYear must between 1 and 366, was "${dayOfYear}"`));
+      });
+    });
+  });
+
+  describe("add", function() {
+    [
+      // Normal year - plural
+      ['2019-01-01T00:00:00.000Z', 3, 'milliseconds'],
+      ['2019-01-01T00:00:00.000Z', 3, 'seconds'],
+      ['2019-01-01T00:00:00.000Z', 3, 'minutes'],
+      ['2019-01-01T00:00:00.000Z', 3, 'days'],
+      ['2019-01-01T00:00:00.000Z', 3, 'weeks'],
+      ['2019-01-01T00:00:00.000Z', 3, 'months'],
+      ['2019-01-01T00:00:00.000Z', 3, 'years'],
+
+      // Leap year - singular
+      ['2020-01-01T00:00:00.000Z', 1, 'millisecond'],
+      ['2020-01-01T00:00:00.000Z', 1, 'second'],
+      ['2020-01-01T00:00:00.000Z', 1, 'minute'],
+      ['2020-01-01T00:00:00.000Z', 1, 'day'],
+      ['2020-01-01T00:00:00.000Z', 1, 'week'],
+      ['2020-01-01T00:00:00.000Z', 1, 'month'],
+      ['2020-01-01T00:00:00.000Z', 1, 'year'],
+
+      // negative values
+      ['2020-01-01T00:00:00.000Z', -3, 'milliseconds'],
+      ['2020-01-01T00:00:00.000Z', -3, 'seconds'],
+      ['2020-01-01T00:00:00.000Z', -3, 'minutes'],
+      ['2020-01-01T00:00:00.000Z', -3, 'days'],
+      ['2020-01-01T00:00:00.000Z', -3, 'weeks'],
+      ['2020-01-01T00:00:00.000Z', -3, 'months'],
+      ['2020-01-01T00:00:00.000Z', -3, 'years'],
+    ].forEach(([dateString, value, unit]) => {
+      it(`"add(new Date('${dateString}'), ${value}, '${unit}'})" should be equal "moment('${dateString}').add(${value}, '${unit}')"`, function() {
+        const nativeDate = new Date(dateString);
+        add(nativeDate, value, unit);
+
+        assert.equal(
+          nativeDate.toISOString(),
+          moment(dateString).add(value, unit).toDate().toISOString()
+        );
+      });
+    });
+  });
+
+  describe("substract", function() {
+    [
+      // Normal year - plural
+      ['2019-01-01T00:00:00.000Z', 3, 'milliseconds'],
+      ['2019-01-01T00:00:00.000Z', 3, 'seconds'],
+      ['2019-01-01T00:00:00.000Z', 3, 'minutes'],
+      ['2019-01-01T00:00:00.000Z', 3, 'days'],
+      ['2019-01-01T00:00:00.000Z', 3, 'weeks'],
+      ['2019-01-01T00:00:00.000Z', 3, 'months'],
+      ['2019-01-01T00:00:00.000Z', 3, 'years'],
+
+      // Leap year - singular
+      ['2020-01-01T00:00:00.000Z', 1, 'millisecond'],
+      ['2020-01-01T00:00:00.000Z', 1, 'second'],
+      ['2020-01-01T00:00:00.000Z', 1, 'minute'],
+      ['2020-01-01T00:00:00.000Z', 1, 'day'],
+      ['2020-01-01T00:00:00.000Z', 1, 'week'],
+      ['2020-01-01T00:00:00.000Z', 1, 'month'],
+      ['2020-01-01T00:00:00.000Z', 1, 'year'],
+
+      // negative values
+      ['2020-01-01T00:00:00.000Z', -3, 'milliseconds'],
+      ['2020-01-01T00:00:00.000Z', -3, 'seconds'],
+      ['2020-01-01T00:00:00.000Z', -3, 'minutes'],
+      ['2020-01-01T00:00:00.000Z', -3, 'days'],
+      ['2020-01-01T00:00:00.000Z', -3, 'weeks'],
+      ['2020-01-01T00:00:00.000Z', -3, 'months'],
+      ['2020-01-01T00:00:00.000Z', -3, 'years'],
+    ].forEach(([dateString, value, unit]) => {
+      it(`"subtract(new Date('${dateString}'), ${value}, '${unit}'})" should be equal "moment('${dateString}').subtract(${value}, '${unit}')"`, function() {
+        const nativeDate = new Date(dateString);
+        subtract(nativeDate, value, unit);
+
+        assert.equal(
+          nativeDate.toISOString(),
+          moment(dateString).subtract(value, unit).toDate().toISOString()
+        );
       });
     });
   });
