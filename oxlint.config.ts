365f7498562f9fab21fc5cb7fbb6dcbde3668409
Tomina <tomina@vycital.eu>
2026-07-28 17:59:29 +0200

    chore(lint): migrate to shared configs from vis-dev-utils

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "oxlint";
 import type { OxlintConfig } from "oxlint";
 
+import oxlintSharedConfig from "vis-dev-utils/oxlint-shared-config";
 import { ignorePatterns } from "./linting-and-formatting-ignore-patterns.ts";
 
 export default defineConfig<OxlintConfig>({
-  plugins: ["eslint", "import", "oxc", "oxc", "typescript", "unicorn"],
-  categories: {
-    correctness: "error",
-    suspicious: "error",
-  },
+  extends: [oxlintSharedConfig],
   rules: {
-    // Enabled by the categories but we don't want to enforce
-    "import/no-unassigned-import": "off",
-
     // Enabled by the categories but disabled for now, PRs welcome (even if only partial)
     "eslint/block-scoped-var": "off",
     "eslint/no-console": "off",
@@ -29,32 +23,6 @@ export default defineConfig<OxlintConfig>({
     "unicorn/no-instanceof-builtins": "off",
     "unicorn/no-new-array": "off",
     "unicorn/prefer-add-event-listener": "off",
-
-    // Enabled beyond the categories
-    "eslint/guard-for-in": "error",
-    "eslint/no-array-constructor": "error",
-    "eslint/no-empty": "error",
-    "eslint/no-fallthrough": "error",
-    "eslint/no-regex-spaces": "error",
-    "eslint/no-useless-return": "error",
-    "eslint/sort-imports": ["error", { ignoreDeclarationSort: true }],
-    "eslint/unicode-bom": "error",
-    "import/consistent-type-specifier-style": "error",
-    "import/no-cycle": "error",
-    "import/no-duplicates": "error",
-    "import/no-mutable-exports": "error",
-    "import/no-named-default": "error",
-    "typescript/array-type": "error",
-    "typescript/explicit-member-accessibility": "error",
-    "typescript/no-import-type-side-effects": "error",
-    "typescript/no-non-null-asserted-nullish-coalescing": "error",
-    "typescript/non-nullable-type-assertion-style": "error",
-    "typescript/only-throw-error": "error",
-    "unicorn/no-abusive-eslint-disable": "error",
-    "unicorn/no-length-as-slice-end": "error",
-    "unicorn/prefer-node-protocol": "error",
-    "vitest/consistent-test-it": "error",
-    "vitest/no-test-prefixes": "error",
   },
   overrides: [
     {
